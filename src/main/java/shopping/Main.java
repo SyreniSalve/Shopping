bc3b92c3fa7b2399a6cea85a079299c39cad6124
Simona Liamo <liamosimona@yahoo.com>
2021-10-16 16:45:40 +0300

    First table and queries

--- src/main/java/shopping/Main.java
+++ src/main/java/shopping/Main.java
@@ -1,16 +1,51 @@
 package shopping;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import shopping.exception.ShoppingException;
+
+import java.sql.*;
 
 public class Main {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/db_name";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    private static final String DB_SCHEMA = "shopping";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_PASSWORD = "1234";
 
     public static void main(String[] args) throws SQLException {
-        Connection  connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+        try (Connection  conn = DriverManager.getConnection(DB_URL + DB_SCHEMA,DB_USER,DB_PASSWORD)){
+            insertNewProduct(conn);
+            listProducts(conn);
+        }
 
+    }
+    private static void insertNewProduct(Connection connection){
+        try (Statement stmt = connection.createStatement()) {
+            String sql = "INSERT INTO products (name) VALUES ('test')";
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            throw new ShoppingException(e);
+        }
+    }
+    private static void listProducts(Connection connection){
+        try (Statement stmt = connection.createStatement()) {
+            String sql = "SELECT * FROM products";
+            try (ResultSet rs = stmt.executeQuery(sql)){
+                listProducts(rs);
+            }
+        } catch (SQLException e) {
+            throw new ShoppingException(e);
+        }
+    }
+    private static void listProducts(ResultSet resultSet){
+        try {
+            while (resultSet.next()){
+                System.out.println("_________________________________");
+                System.out.println(resultSet.getInt("id"));
+                System.out.println(resultSet.getString("name"));
+                System.out.println(resultSet.getTimestamp("created"));
+                System.out.println("_________________________________");
+            }
+        } catch (SQLException e) {
+            throw new ShoppingException(e);
+        }
     }
 }
