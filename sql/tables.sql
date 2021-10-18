5a8b1db7b5e9da900fcf139597cd63f944ce2b82
Simona Liamo <liamosimona@yahoo.com>
2021-10-18 21:52:35 +0300

    Fix diagram and add tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -10,13 +10,13 @@ CREATE TABLE `products` (
   CHANGE COLUMN `name` `name` VARCHAR(200) NOT NULL ,
   CHANGE COLUMN `created` `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
-  CREATE TABLE `orders` (
+CREATE TABLE `orders` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `status` VARCHAR(45) NOT NULL DEFAULT 'NEW',
     `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`));
 
-  CREATE TABLE `order_items` (
+CREATE TABLE `order_items` (
     `order_id` INT NOT NULL,
     `product_id` INT NOT NULL,
     `quantity` INT NOT NULL DEFAULT 1,
@@ -33,3 +33,48 @@ CREATE TABLE `products` (
       ON DELETE NO ACTION
       ON UPDATE NO ACTION);
 
+CREATE TABLE `shopping`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(60) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `shopping`.`address_book` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `country` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `state` VARCHAR(45) NOT NULL,
+  `street1` VARCHAR(45) NOT NULL,
+  `street2` VARCHAR(45) NULL,
+  `post` VARCHAR(45) NOT NULL,
+  `user_id` INT NOT NULL,
+  `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_address_book_users_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_address_book_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `shopping`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `shopping`.`reviews` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `rating` INT NULL,
+  `review` VARCHAR(200) NULL,
+  `product_id` INT NOT NULL,
+  `order_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_reviews_orders_idx` (`order_id` ASC) VISIBLE,
+  INDEX `fk_reviews_products_idx` (`product_id` ASC) VISIBLE,
+  CONSTRAINT `fk_reviews_orders`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `shopping`.`orders` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_reviews_products`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `shopping`.`products` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
